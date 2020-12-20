62f6d7496a410038a68547417c1d4323425eb8d5
LisetAlfaro <lisetalfarogonzalez@gmail.com>
2020-12-20 04:28:42 -0500

    making cleaner the code

--- utils.pl
+++ utils.pl
@@ -9,27 +9,27 @@ altern([X|L],[Y|M],N):-
 % delete the element in the X position and left it in Y, the rest of the
 % list is in L.
 dIndex(X,[Y|L],X,Y,L).
-dIndex(X,[F|G],Xa,Y,L):- 
-    S is Xa+1, 
+dIndex(X,[F|G],Xa,Y,L):-
+    S is Xa+1,
     dIndex(X,G,S,Y,R),
     append([F],R,L).
 
 %return the element in the list
 takeIndex(0,[R|_],R):-!.
-takeIndex(X,[_|L],Z):- 
+takeIndex(X,[_|L],Z):-
     X1 is X-1,
     takeIndex(X1,L,Z).
 
 %hace una lista de Y elementos todos X.
 genList(_,0,[]):-!.
-genList(X,Y,[X|N]):- 
+genList(X,Y,[X|N]):-
     plus(Y1,1,Y),
     genList(X,Y1,N).
 
 % gived P[(X,Y)] create a list T where there are repliacted an Y times
 % each X number.
 realList([],[]).
-realList([(X,Y)|P],T):- 
+realList([(X,Y)|P],T):-
     genList(X,Y,M),
     realList(P,G),
     append(M,G,T).
@@ -38,7 +38,7 @@ realList([(X,Y)|P],T):-
 % elements of the first argument.
 take(_,0,[]).
 take([],_,[]).
-take([Y|L1],X,[Y|L2]):- 
+take([Y|L1],X,[Y|L2]):-
     plus(Z,1,X),
     take(L1,Z,L2),
     !.
@@ -66,31 +66,32 @@ repart(L,[C|J]):-
 %stablish the order there is not working as predicade.
 sortOrder(_,[],[]).
 sortOrder(X,[X|Y],[X|Y]).
-sortOrder(X,[Y|L],Z):- 
-    append(L,[Y],R), 
+sortOrder(X,[Y|L],Z):-
+    append(L,[Y],R),
     sortOrder(X,R,Z).
 
 %gets the bigger element of a list.
 greaters([X],X).
-greaters([X|Xs],X):- 
+greaters([X|Xs],X):-
     greaters(Xs,Y),
     X > Y,
     !.
-greaters([X|Xs],Y):- 
+greaters([X|Xs],Y):-
     greaters(Xs,Y),
     Y >= X,
     !.
-
+% return the first element of the tuple that contains a 0 as the third
+% element in a list
 pickCero([(Ca,_,0)|_],Ca):- !.
 pickCero([_|L],R):- pickCero(L,R).
 
-% gets the bigger element of a list of two element based on the second one.
+%return the tuple with the grater second element
 greaterSecond([X],X).
-greaterSecond([(X,Y)|Xs],(X,Y)):- 
+greaterSecond([(X,Y)|Xs],(X,Y)):-
     greaterSecond(Xs,(_,W)),
     Y > W,
     !.
-greaterSecond([(_,Y)|Xs],(V,W)):- 
+greaterSecond([(_,Y)|Xs],(V,W)):-
     greaterSecond(Xs,(V,W)),
     W >= Y,
     !.
@@ -109,19 +110,41 @@ myabs(X,Y):-
     X < 0 -> Y is -X;
     Y = X.
 
-stractSecondItem([],[]).
-stractSecondItem([(_,Y)|Ls],[Y|L]):- stractSecondItem(Ls,L).
+%stractSecondItem([],[]).
+%stractSecondItem([(_,Y)|Ls],[Y|L]):- stractSecondItem(Ls,L).
 
-stractFirstItem([],[]).
-stractFirstItem([(X,_)|Ls],[X|L]):- stractFirstItem(Ls,L).
+%stractFirstItem([],[]).
+%stractFirstItem([(X,_)|Ls],[X|L]):- stractFirstItem(Ls,L).
 
-gFirstList(Y,[(X,Y)|_],X).
-gFirstList(Y,[(_,_)|L],X):- gFirstList(Y,L,X).
+%gFirstList(Y,[(X,Y)|_],X).
+%gFirstList(Y,[(_,_)|L],X):- gFirstList(Y,L,X).
 
-gSecondList(X,[(X,Y)|_],Y).
-gSecondList(X,[(_,_)|L],Y):- gSecondList(X,L,Y).
+%gSecondList(X,[(X,Y)|_],Y).
+%gSecondList(X,[(_,_)|L],Y):- gSecondList(X,L,Y).
 
 allCero([0]):-!.
 allCero([0|P]):-
     allCero(P),
     !.
+
+addList([],0):-!.
+addList([X|L],C):-
+    addList(L,C1),
+    C is C1 + X.
+min(A,B,C):-
+    A > B -> C is B;
+    C is A.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
