8561ee17e6d878b163a9af2a2a99971e71f32a26
LisetAlfaro <lisetalfarogonzalez@gmail.com>
2020-12-20 03:57:42 -0500

    fixing errors calculating points
    1-in canReachHorizontal and canReachVertical added the checking if the cell is occupated
    2-In horizontalNext and verticalNext changed the list of possible positions, the oldest doesn't include the 0
    3-In setpoint included the option with negative numbers
    4- In updatePoints there was an unnecessary increment.

--- puntuation.pl
+++ puntuation.pl
@@ -1,4 +1,4 @@
-canReachHorizontal(_,_,C,C):-!.
+canReachHorizontal(P,R,C,C):- board(P,R,C,1),!.
 canReachHorizontal(Player,R,C,Goal):-
     C < Goal,%moverme a la derecha
     board(Player,R,C,1),%esta activa?
@@ -10,7 +10,7 @@ canReachHorizontal(Player,R,C,Goal):-
     C1 is C - 1,
     canReachHorizontal(Player,R,C1,Goal).
 
-canReachVertical(_,R,-,R):-!.
+canReachVertical(P,R,C,R):- board(P,R,C,1),!.
 canReachVertical(Player,R,C,Goal):-
     R < Goal,%moverme up
     board(Player,R,C,1),%it is active?
@@ -24,25 +24,34 @@ canReachVertical(Player,R,C,Goal):-
 
 horizontalNext(Player,R,C,A):-
     findall(1,
-           (member(Goal,[1,2,3,4,5]),
+           (member(Goal,[0,1,2,3,4]),
             Goal =\= C,
             canReachHorizontal(Player,R,C,Goal)),
             Nexts),
     length(Nexts,A).
 verticalNext(Player,R,C,A):-
     findall(1,
-           (member(Goal,[1,2,3,4,5]),
+           (member(Goal,[0,1,2,3,4]),
             Goal =\= R,
             canReachVertical(Player,R,C,Goal)),
             Nexts),
     length(Nexts,A).
 
 setpoint(Player,C):-
+    C > 0,
     points(Player,C1),
     Cf is C1 + C,
     retractall(points(Player,C1)),
     T=..[points,Player,Cf],
     assert(T).
+setpoint(Player,C):-
+    points(Player,C1),
+    myabs(C,X),
+    Cf is C1 - X,
+    retractall(points(Player,C1)),
+    T=..[points,Player,Cf],
+    assert(T).
+
 
 updatePoints(Player,R,C):-
     horizontalNext(Player,R,C,C1),
@@ -52,5 +61,19 @@ updatePoints(Player,R,C):-
     (C2 > 0 -> Y is C2 + 1;
     Y = 0),
     A is Z+Y,
-    (A > 0 -> plus(A,1,A1),setpoint(Player,A1);
+    (A > 0 -> setpoint(Player,A);
     setpoint(Player,1)).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
